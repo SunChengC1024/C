6147ab3c4c871c17825292fb5582ba47152989c0
Black1024 <sc1685975437@outlook.com>
2017-03-05 15:26:23 +0800

    作业01-1
    Signed-off-by: Black1024 <sc1685975437@outlook.com>

---
+++ 向C语言问好.c
@@ -0,0 +1,16 @@
+/*
+    说明：任务01_1 向C语言问好
+    姓名：孙程程
+    学号：2016011442
+    班级：3班
+    日期：2017/03/03
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+        printf("Nice to meet you, C! I am SunChengCheng \n");//输出带有姓名的向C语言问好语句
+
+        return 0;
+}
